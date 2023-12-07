cc2f4e5b1ce1d805e0c55fc4805311e7bc52432e
Samyydjemai <samygamiing@gmail.com>
2023-12-07 20:38:56 +0100

    Add project files.

---
+++ Services/Villes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAp.Services
+{
+    internal class Ville
+    {
+        public class Villes
+        {
+            public List<string> LsVille;
+            public Villes()
+            {
+
+                LsVille = new List<string>();
+
+                LsVille.Add("Annecy");
+                LsVille.Add("Marseille");
+                LsVille.Add("Lyon");
+                LsVille.Add("Paris");
+            }
+        }
+    }
+}
